1a76cdb45d288b8d378997d526db8d372040ec2a
Matheusfnf <matheusfortunatoaw@gmail.com>
2022-09-26 13:59:38 -0300

    versaofinal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import "./palavras.js";
+import imagemforcainicial from "./images/forca0.png";
+import primeiraimagem from "./images/forca1.png";
+import segundaimagem from "./images/forca2.png";
+import terceiraimagem from "./images/forca3.png";
+import quartaimagem from "./images/forca4.png";
+import quintaimagem from "./images/forca5.png";
+import sextaimagem from "./images/forca6.png";
+import palavras from "../src/palavras.js";
+import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [ativo, setAtivo] = useState(false);
+  const [palavraembaralhada, setPalavraEmbaralhada] = useState("");
+  const [erros, setErros] = useState(0);
+  const [imagem, setImagem] = useState(imagemforcainicial);
+  const [perdeu, setPerdeu] = useState("");
+  const [vitoria, setVitoria] = useState("");
+  const [textochute, settextochute] = useState("");
+  const [words, setWords] = useState({
+    a: false,
+    b: false,
+    c: false,
+    d: false,
+    e: false,
+    f: false,
+    g: false,
+    h: false,
+    i: false,
+    j: false,
+    k: false,
+    l: false,
+    m: false,
+    n: false,
+    o: false,
+    p: false,
+    q: false,
+    r: false,
+    s: false,
+    t: false,
+    u: false,
+    v: false,
+    w: false,
+    x: false,
+    y: false,
+    z: false,
+  });
+  useEffect(() => {
+    if (erros == 6) {
+      setImagem(sextaimagem);
+      mensagemperdeu();
+    }
+    if (erros == 5) setImagem(quintaimagem);
+    if (erros == 4) setImagem(quartaimagem);
+    if (erros == 3) setImagem(terceiraimagem);
+    if (erros == 2) setImagem(segundaimagem);
+    if (erros == 1) setImagem(primeiraimagem);
+    return;
+  }, [erros]);
+
+  function mensagemperdeu() {
+    return setPerdeu(
+      <div className="perdeu">
+        Você <span>perdeu</span>, a palavra correta era:
+        <span className="underline"> {palavraembaralhada}</span>
+      </div>
+    );
+  }
+
+  function mudouinput() {
+    if (palavraembaralhada == textochute) {
+      setVitoria(
+        <div className="ganhou">
+          <span>Parabéns</span>, a palavra correta era{" "}
+          <span className="palavraganhou">{palavraembaralhada} </span>
+        </div>
+      );
+      setAtivo(false);
+      setWord(palavraembaralhada);
+    } else {
+      setImagem(sextaimagem);
+      setAtivo(false);
+      setWord(palavraembaralhada);
+      setPerdeu(
+        <div className="perdeu">
+          Você <span>perdeu</span>, a palavra correta era {palavraembaralhada}{" "}
+        </div>
+      );
+    }
+  }
+
+  function pegarvalor(e) {
+    settextochute(e.target.value.toLowerCase());
+  }
+
+  function embaralhapalavra() {
+    setAtivo(true);
+    const palavraEscolhida =
+      palavras[Math.floor(Math.random() * palavras.length)];
+    const palavranormalizada = palavraEscolhida
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    console.log(palavraEscolhida);
+
+    setPalavraEmbaralhada(palavraEscolhida);
+
+    let palavratrocada = "";
+    for (let i = 0; i < palavraEscolhida.length; i++) {
+      palavratrocada = palavratrocada + "-";
+    }
+    return setWord(palavratrocada);
+  }
+
+  const checaPalavra = (e) => {
+    const palavraComTracos = [...word];
+    if (palavraembaralhada.includes(e)) {
+      for (let i = 0; i < palavraembaralhada.length; i++) {
+        if (palavraembaralhada[i] === e) {
+          palavraComTracos[i] = e;
+          setWord(palavraComTracos.join(""));
+        }
+      }
+    } else {
+      const erroSomado = erros + 1;
+      setErros(erroSomado);
+    }
+    if (!palavraComTracos.includes("-")) {
+      setVitoria(
+        <div className="ganhou">
+          <span>Parabéns</span>, a palavra correta era{" "}
+          <span className="palavraganhou">{palavraembaralhada} </span>
+        </div>
+      );
+      setAtivo(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="estadoinicial">
+        <div className="forcaeescolherpalavra">
+          <img className="imginicial" src={imagem} alt="" />
+          <div onClick={() => embaralhapalavra()} className="escolherpalavra">
+            Escolher Palavra
+          </div>
+        </div>
+        <div>
+          {perdeu}
+          {vitoria}
+        </div>
+        <p className="palavraunderline">{word}</p>
+        <div className="letras-alfabeto">
+          {!ativo
+            ? Object.keys(words).map((e, index) => {
+                return (
+                  <button
+                    disabled
+                    className="letra"
+                    onClick={() => {
+                      setWords((prevState) => {
+                        const state = { ...prevState };
+                        state[e] = !state[e];
+                        return { ...state };
+                      });
+                      checaPalavra(e);
+                    }}
+                    key={index}
+                  >
+                    {e.toUpperCase()}
+                  </button>
+                );
+              })
+            : Object.keys(words).map((e, index) => {
+                return (
+                  <button
+                    disabled={words[e]}
+                    className="letra"
+                    onClick={() => {
+                      setWords((prevState) => {
+                        const state = { ...prevState };
+                        state[e] = !state[e];
+                        return { ...state };
+                      });
+                      checaPalavra(e);
+                    }}
+                    key={index}
+                  >
+                    {e.toUpperCase()}
+                  </button>
+                );
+              })}
+        </div>
+
+        <div className="OpcaoChutar">
+          <div className="JaSeiAPalavra">Já sei a palavra!</div>
+          <input
+            className="inputchute"
+            type="text"
+            name="input"
+            onChange={(e) => {
+              pegarvalor(e);
+            }}
+          />
+
+          <button onClick={mudouinput} className="Chutar">
+            Chutar{" "}
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
